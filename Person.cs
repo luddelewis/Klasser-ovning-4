0077700fab8cfab122d8470585c7cda8a3691dcc
luddelewis <74183690+luddelewis@users.noreply.github.com>
2021-09-02 17:47:44 +0200

    Initial Commit

---
+++ Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser_övning_2
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return FirstName + " " + LastName; } }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public bool IsOfAge { get { return AgeCheck(DateOfBirth);  } }
+        public bool AgeCheck(DateTime BDay)         //Function for checking age
+        {
+            if (DateTime.Now.Year - DateOfBirth.Year > 18)
+            {
+                return true;
+            }
+            else if (DateTime.Now.Year - DateOfBirth.Year == 18 && DateOfBirth.DayOfYear <= DateTime.Now.DayOfYear)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public double Heigth { get; set; }
+
+        public double Weight { get; set; }
+
+        public double BMI { get { return Weight / Math.Pow(Heigth,2) ; } }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser_övning_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person User = new Person();
+
+            User.FirstName = TakeInput("Your first name?");
+            User.LastName = TakeInput("Your last name?");
+            Console.WriteLine("Your full name is: " + User.FirstName + " " + User.LastName);
+
+            User.DateOfBirth = Convert.ToDateTime(TakeInput("Your date of birth? (YYYY-MM-DD)"));
+            
+            if (User.IsOfAge) Console.WriteLine("You are of age!");
+            else Console.WriteLine("You are not of age!");
+
+            User.Weight = int.Parse(TakeInput("Your weight? (kg)"));
+            User.Heigth = int.Parse(TakeInput("Your height? (m)"));
+            Console.WriteLine("Your BMI is: " + User.BMI);
+
+
+            Console.ReadLine();
+        }
+
+        static string TakeInput(string query)       //Function that makes sure an input isn't empty
+        {
+            Console.WriteLine(query);
+            string Answer;
+            while (true)
+            {
+                Answer = Console.ReadLine();
+                if (Answer != string.Empty) break;
+            }
+                return Answer;
+        }
+       
+
+    }
+}
